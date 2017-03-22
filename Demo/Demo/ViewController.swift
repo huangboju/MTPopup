8b91f190256f5196609ba6297ba41c14f59297cf
huangboju <529940945@qq.com>
2017-03-22 15:30:43 +0800

    重构项目

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -9,27 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     fileprivate lazy var tableView: UITableView = {
         let tableView = UITableView(frame: self.view.bounds, style: .grouped)
         tableView.dataSource = self
         tableView.delegate = self
         return tableView
     }()
-
+    
     lazy var controllers: [UIViewController.Type] = [
         PopupViewController1.self,
         PopupViewController1.self,
         BottomSheetController.self
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "STPopupController"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         view.addSubview(tableView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -40,21 +40,21 @@ extension ViewController: UITableViewDataSource {
     func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
         return controllers.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
     }
 }
 
 extension ViewController: UITableViewDelegate {
-
+    
     func tableView(_: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.textLabel?.text = "\(controllers[indexPath.row].classForCoder())"
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
-
+        
         let popupController = STPopupController(rootViewController: controllers[indexPath.row].init())
 
         if indexPath == IndexPath(row: 1, section: 0) {
--- MTPopup.podspec
+++ MTPopup.podspec
@@ -1,14 +1,14 @@
 
 Pod::Spec.new do |s|
 
-  s.name         = "SwiftySTPopup"
+  s.name         = "MTPopup"
   s.version      = "1.0.0"
   s.summary      = "You will like it"
-  s.homepage     = "https://github.com/huangboju/SwiftySTPopup"
+  s.homepage     = "https://github.com/huangboju/MTPopup"
   s.license      = "MIT"
   s.author       = { "huangboju" => "529940945@qq.com" }
   s.platform     = :ios,'8.0'
-  s.source       = { :git => "https://github.com/huangboju/SwiftySTPopup.git", :tag => "#{s.version}" }
+  s.source       = { :git => "https://github.com/huangboju/MTPopup.git", :tag => "#{s.version}" }
   s.source_files  = "STPopup/Classes/**/*.swift"
   s.framework  = "UIKit"
   s.requires_arc = true
